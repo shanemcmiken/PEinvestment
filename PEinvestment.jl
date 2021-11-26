554ad7cd58113964711a76a25f14fb296bf435ad
shanemcmiken <56058438+shanemcmiken@users.noreply.github.com>
2021-11-26 16:03:55 -0500

    add plots

--- PEinvestment.jl
+++ PEinvestment.jl
@@ -30,7 +30,7 @@ r   = 0.04
 
 #steady state
 kbar = (α/(p*(r+δ)))^(1.0/(1.0-α))
-Vbar = ((1+r)/r)*(kbar^α - δ*kbar)
+Vbar = ((1.0+r)/r)*(kbar^α - δ*kbar)
 
 #--------------------------------#
 #  GRIDS and TRANSITION MATRIX
@@ -82,7 +82,8 @@ zgrid = exp.(zgrid) # exponentiate grid
 # capital grid
 nk = 100
 kmax = ((maximum(zgrid)*α)/(p*(r+δ)))^(1.0/(1.0-α))
-kgrid = [kmax*(1-δ)^i for i∈0:(nk-1)]
+kgrid = [kmax*(1-δ)^(nk-i-1) for i∈0:(nk-1)]
+
 
 #--------------------------------#
 #  VALUE FUNCTION ITERATION
@@ -138,8 +139,8 @@ function value(currentstate::state)
   # compute return function
   R       = zc*kc^α .- p.*kgrid .+ p*(1.0-δ)*kc .- γ_c/2.0 .* ((kgrid .- (1.0-δ)*kc)./kc).^2 .*kc - γ_f.*zc.*kc^α.*(kgrid.>(1-δ)*kc)
 
-  # compute new value funciton
-  VVp     = R .+ (1.0+r).^(-1) .* cont
+  # compute new value funciton - if return is less than zero than value is -Inf
+  VVp     = R .+ (1.0+r).^(-1) .* cont - Inf*(R.<0) 
 
   # find maximum
   Vnew, tempidx = findmax(vec(VVp))
@@ -196,7 +197,7 @@ function vfi(Vbar::Float64, zgrid::Array, kgrid::Array, P::Array; α = α, r = r
       R = zzgr.*kkgr.^α .- p.*kgrid[idx].+ p.*(1.0-δ).*kkgr .- γ_c/2.0 .* ((kgrid[idx] .- (1.0-δ)*kkgr)./kkgr).^2 .*kkgr - γ_f.*zzgr.*kkgr.^α.*(kgrid[idx].>(1-δ)*kkgr)
 
       # Re-compute value function under conjectured optimal policy
-      V_next = R + (1.0+r).^(-1) .* evp_k
+      V_next = R + (1.0+r).^(-1) .* evp_k - Inf*(R.<0)  
 
     end
 
@@ -209,13 +210,14 @@ function vfi(Vbar::Float64, zgrid::Array, kgrid::Array, P::Array; α = α, r = r
     print("iteration: ",iter, ", critical val: ", crit*1.0e5," times 10^5", "\n")
   end  
 
+  index = convert(Vector{Int64}, index)
   return V_next, index, crit
 end
 
 @time valfunc, index, crit = vfi(Vbar,zgrid,kgrid,P)
 
 #------------------------------------------#
-#  VALUE FUNCTION ITERATION : V2-VECTORIZED
+#  VALUE FUNCTION ITERATION : VECTORIZED
 #------------------------------------------#
 
 function vfivec(Vbar::Float64, zgrid::Array, kgrid::Array, P::Array; α = α, r = r, δ = δ, γ_c = γ_c, γ_f = γ_f, p = p,crit = Inf, maxiter = 10000, iter = 0, tol = 1.0e-5, nfix = 10)
@@ -238,7 +240,7 @@ function vfivec(Vbar::Float64, zgrid::Array, kgrid::Array, P::Array; α = α, r 
     if mod(iter,nfix) == 0 # Howard acceleration
       
       # compute (nz*nk*nk)-dimensional value function
-      obj = R + (1.0/(1.0+r)).*repeat(P*V_init,nk)
+      obj = R + (1.0/(1.0+r)).*repeat(P*V_init,nk) - Inf*(R.<0)
 
       # Get maximum
       V_next, index = findmax(obj, dims = 2)
@@ -261,7 +263,7 @@ function vfivec(Vbar::Float64, zgrid::Array, kgrid::Array, P::Array; α = α, r 
       R_idx = zzgr.*kkgr.^α .- p.*kgrid[idx].+ p.*(1.0-δ).*kkgr .- γ_c/2.0 .* ((kgrid[idx] .- (1.0-δ)*kkgr)./kkgr).^2 .*kkgr - γ_f.*zzgr.*kkgr.^α.*(kgrid[idx].>(1-δ)*kkgr)
 
       # Re-compute value function under conjectured optimal policy
-      V_next = R_idx + (1.0+r).^(-1) .* evp_k
+      V_next = R_idx + (1.0+r).^(-1) .* evp_k - Inf*(R_idx.<0) 
 
     end
 
@@ -285,11 +287,10 @@ end
 #  ERGODIC DISTRIBUTION - Young (2010)
 #------------------------------------------#
 
-function young(index::Array, P::Array; crit = Inf, maxiter = 10000, iter = 0, tol = 1.0e-5)
+function young(index::Array, P::Array; crit = Inf, maxiter = 10000, iter = 0, tol = 1.0e-6)
 
-  # intialise masses
+  # initialise mass
   μ_init = fill((1/(nz*nk)), (nz,nk))
-  μ_next = zeros(nz,nk)
 
   while (crit > tol && iter < maxiter)
     
@@ -300,11 +301,8 @@ function young(index::Array, P::Array; crit = Inf, maxiter = 10000, iter = 0, to
     for ind = 1:(nz*nk)
       kk = convert(Int, floor((ind-0.05)/nz))+1
       zz = convert(Int, floor(mod(ind-0.05,nz))+1)
-      μ_next[zz,index[ind]] =  μ_next[zz,index[ind]] + μ_init[zz,kk] 
+      μ_next[zz,index[ind]] =  μ_next[zz,index[ind]] + P[:,zz]'*μ_init[:,kk] 
     end
-
-    # update realisation of productivity next period 
-    μ_next = P*μ_next
     
     # check tolerance
     crit = maximum(abs.(μ_init .- μ_next))
@@ -315,13 +313,114 @@ function young(index::Array, P::Array; crit = Inf, maxiter = 10000, iter = 0, to
 
   end
 
-  return μ_next, crit
+  return μ_init, crit, iter
 end
 
-μ_ergo, crit = young(index,P)
-
+@time μ_ergodic, crit, iterfin = young(index,P)
 
 #------------------------------------------#
 #  EXTRACTING RESULTS FROM THE MODEL
 #------------------------------------------#
+
+## (a)
+#plot ergodic distribution
+ergodicdist = heatmap(μ_ergodic, color = :greys)
+savefig(ergodicdist,"./ergodicdistribution.png")
+display(ergodicdist)
+
+#plot policy function
+pols = reshape(index,(nz,nk)) # policies
+zlow = pols[cover-2,:] # fix productivity at low
+zmid = pols[cover+1,:] # fix productivity at steady state
+zhigh = pols[cover+4,:] # fix productivity high
+
+p1 = plot(kgrid,kgrid[zlow], ylabel = "low productivity", title = "k policy")
+p2 = plot(kgrid,kgrid[zmid], ylabel = "steady state productivity")
+p3 = plot(kgrid,kgrid[zhigh], ylabel = "high productivity")
+p4 = heatmap(pols, color = :greys, ylabel = "Z", xlabel = "K")
+
+policyfunctions = plot(p1,p2,p3,p4, layout = (2,2), legend = false)
+savefig(policyfunctions,"./policyfunctions.png")
+display(policyfunctions)
+
+#plot value function 
+p5 = plot(kgrid,valfunc[cover-2,zlow], ylabel = "low productivity", title = "Value Function")
+p6 = plot(kgrid,valfunc[cover+1,zlow], ylabel = "steady state productivity")
+p7 = plot(kgrid,valfunc[cover+4,zlow], ylabel = "high productivity")
+p8 = heatmap(valfunc, color = :greys, ylabel = "Z", xlabel = "K")
+
+valuefunction = plot(p5,p6,p7,p8, layout = (2,2), legend = false)
+savefig(valuefunction,"./valuefunction.png")
+display(valuefunction)
+
+## (b) marginal Distributions
+marginal_k = sum(μ_ergodic, dims = 1)
+marginal_z = sum(μ_ergodic, dims = 2)
+p9 = plot(zgrid, marginal_z, ylabel = "probability density", title = "marginal productivity distribution" )
+p10 = plot(kgrid, marginal_k', title = "marginal capital distribution" )
+
+marginaldistributions = plot(p9,p10, layout = (1,2), legend = false)
+savefig(marginaldistributions,"./marginaldistributions.png")
+display(marginaldistributions)
+
+## (c)
+kkgr = log.(repeat(kgrid',nz))
+zzgr = log.(repeat(zgrid,1,nk))
+yygr = log.(exp.(zzgr).*exp.(kkgr).^α)
+iigr = (kgrid[pols]-(1-δ).*exp.(kkgr))./exp.(kkgr)
+
+# mean
+X = [kkgr, zzgr, yygr, iigr]
+EX = zeros(size(X),)
+for (ii, x) in enumerate(X)
+  EX[ii] = sum(x.*μ_ergodic)
+end
+
+#variance
+STDX = zeros(size(X),)
+for (ii, x) in enumerate(X)
+  STDX[ii] = sum(x.^2 .*μ_ergodic) - EX[ii].^2
+end 
+
+# correlation matrix
+CORRX = zeros(size(X)[1],size(X)[1])
+for (ii,xi) in enumerate(X)
+  for (jj,xj) in enumerate(X)
+    CORRX[ii,jj] = (sum(xi.*xj.*μ_ergodic) - EX[ii]*EX[jj])/(sqrt.(VARX[ii])*sqrt.(VARX[jj]))
+  end
+end
+
+sum(X*X.μ_ergodic)
+
+## (d) obtain aggregates
+kkgr = repeat(kgrid',nz)
+zzgr = repeat(zgrid,1,nk)
+yygr = zzgr.*kkgr.^α
+iigr = (kgrid[pols]-(1-δ).*kkgr)./(kkgr)
+vvgr = copy(valfunc)
+
+# mean
+X = [kkgr, zzgr, yygr, iigr, vvgr]
+EX = zeros(size(X),)
+for (ii, x) in enumerate(X)
+  EX[ii] = sum(x.*μ_ergodic)
+end
+
+## (e) 
+TFP = EX[3]/EX[1]^α
+
+## (f) implied TFP is larger than average of true underlying productivity because larger (more productive) firms produce more.
+
+## (g)
+#find coordinates of maximal mass
+val, cord = findmax(μ_ergodic)
+p11 = plot(zgrid./kgrid[cord[2]],kgrid[pols[:,cord[2]]], ylabel = "k policy", xlabel = "z/k")
+eqpolicy = plot(p11, legend = false)
+savefig(eqpolicy,"./eqpolicy.png")
+display(eqpolicy)
+
+
+
+
+
 
